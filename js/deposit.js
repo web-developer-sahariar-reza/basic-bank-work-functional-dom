31e2b8ad34b74ad7c12780f398b5c4174e4fae92
web-developer-sahariar-reza <web-developer-sahariar-reza@outlook.com>
2023-01-18 21:10:17 +0600

    utilities and deposit js work done

---
+++ js/deposit.js
@@ -0,0 +1,56 @@
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  /* 
+Function Name: getInputValueById()
+    1. get the deposit-field id
+    2. get the value from input field
+    3. convert string value to a number
+*/
+
+  const newDepositAmount = getInputValueById("deposit-field");
+
+  /*
+Function Name: getTextElementValueById()
+    1. get the deposit-total by id
+    2. get the innerText from the deposit-total
+    3. convert string value to a number
+  */
+
+  const previousDepositTotal = getTextElementValueById("deposit-total");
+
+  /*
+    Calculate new deposit total 
+  */
+
+  const newDepositTotal = previousDepositTotal + newDepositAmount;
+
+  /*
+Function Name: setTextElementValueById()
+    1. get the deposit-total by id
+    2. set the new Deposit Total
+  */
+
+  setTextElementValueById("deposit-total", newDepositTotal);
+
+  /*
+Function Name: getTextElementValueById()
+    1. get the balance-total by id
+    2. get the innerText from the balance-total
+    3. convert string value to a number
+  */
+
+  const previousBalanceTotal = getTextElementValueById("balance-total");
+
+  /*
+    Calculate new balance total 
+  */
+
+  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+  /*
+Function Name: setTextElementValueById()
+    1. get the deposit-total by id
+    2. set the new Deposit Total
+  */
+
+  setTextElementValueById("balance-total", newBalanceTotal);
+});
---
+++ js/utilities.js
@@ -0,0 +1,19 @@
+function getInputValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputFieldValueString = inputField.value;
+  const inputFieldValue = parseFloat(inputFieldValueString);
+  inputField.value = "";
+  return inputFieldValue;
+}
+
+function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const TextElementValueString = textElement.innerText;
+  const textElementValue = parseFloat(TextElementValueString);
+  return textElementValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.innerText = newValue;
+}
